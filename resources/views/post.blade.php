459611516ba0a97cd4a97e4fd4d08e5a0b0a0637
Haikaaal <haikal.athallarik@gmail.com>
2024-10-03 13:23:31 +0700

    progress

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,8 +3,11 @@
 
     <article class="py-8 mx-w-screen-md border-b">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{$post['title']}}</h2>
-        <div class="text-base text-gray-500">
-            <a href="#">{{$post['author']}}</a> | {{$post -> created_at -> format('M d, Y')}}
+        <div>
+            By
+            <a href="/authors/{{ $post->author->username }}" class="hover:underline text-base text-gray-500">{{ $post->author->name }}</a> | {{$post -> created_at -> format('M d, Y')}}
+            in
+            <a href="/categories/{{ $post->category->slug }}" class="hover:underline text-base text-gray-500"> {{ $post->category->name }} </a>
         </div>
         <p class="my-4 font-light">{{$post['body']}}</p>
         <a href="/posts/" class="font-medium text-blue-500 hover:underline">&laquo Back to post;</a>
